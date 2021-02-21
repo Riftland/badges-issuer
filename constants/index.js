6e2040dbf4629083c28271817968d88ee5aaf134
Molpe <fran.mscr@gmail.com>
2021-02-21 23:52:13 +0100

    feat:added constants with common paths used and api urls

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+const BADGR_GET_TOKEN_URL = 'https://api.badgr.io/o/token'
+const BADGR_BASIC_URL = 'https://api.badgr.io/v2/badgeclasses'
+
+const EMAILS_PATH = 'emails.json'
+const CREDENTIALS_PATH = 'credentials.json'
+const BADGES_PATH = 'badges.json'
+
+module.exports = {
+  BADGR_GET_TOKEN_URL,
+  BADGR_BASIC_URL,
+  EMAILS_PATH,
+  CREDENTIALS_PATH,
+  BADGES_PATH
+}
